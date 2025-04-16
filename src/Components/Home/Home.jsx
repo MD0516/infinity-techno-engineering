3188ba28d3f5fd1eb811b3851788a745cb8056be
Murali... <msmurali2005@gmail.com>
2025-04-16 11:00:39 +0530

    changed layout

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
                         <h1  className='text-white text-[3rem] font-bold mt-5 mr-5 ml-5 mb-1 pt-5 pr-5 pb-1 pl-5'>Reliable Industrial Engineering <br /> Solutions – All Under One Roof</h1>
                         <p className='text-white text-[1.3rem] m-5 mt-1 mr-5 ml-5 mb-5 pt-1 pr-5 pb-5 pl-5'>Empowering industries with expert support in automation, <br /> electrical, hydraulic, and mechanical systems.</p>
                     </div>
-                    <div>
-                        <a href='#Services' className='bg-white p-5 m-5 rounded-[8px] font-bold text-[var(--secondary)] hover:text-white hover:bg-[var(--secondary)] hover:cursor-pointer'  > Explore Services </a>
+                    <div className='flex flex-col justify-center items-center'>
+                        <a href='#Services' className='bg-white p-5 m-5 rounded-[8px] font-bold text-[var(--secondary)] hover:text-white hover:bg-[var(--secondary)] hover:cursor-pointer'  > Explore Services </a> 
                         <a href='#Contact' className='bg-white p-5 m-5 rounded-[8px] font-bold text-[var(--secondary)] hover:text-white hover:bg-[var(--secondary)] hover:cursor-pointer'> Get in Touch </a>
                     </div>
                 </div>
